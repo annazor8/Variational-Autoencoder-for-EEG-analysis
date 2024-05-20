fd25c60ba6c31b2523802e4e6b14a4dc3cc78a7f
annazor8 <anna.zorzetto.1@studenti.unipd.it>
2024-05-20 15:28:42 +0000

    leave one subj/session + normalization

--- TUAR_dataset.py
+++ TUAR_dataset.py
@@ -17,10 +17,11 @@ from collections import defaultdict
 np.random.seed(43) 
 # Directory containing the EDF files
 #directory_path='C:\Users\albin\OneDrive\Desktop\TCParRidotto'
-directory_path = r'C:\Users\albin\OneDrive\Desktop\TCParRidotto'
+#directory_path = r'C:\Users\albin\OneDrive\Desktop\TCParRidotto'
 
 #directory_path = '/home/azorzetto/data1/TCParRidotto'
-channels_to_set=['EEG FP1-REF', 'EEG FP2-REF', 'EEG F3-REF', 'EEG F4-REF', 'EEG C3-REF', 'EEG C4-REF', 'EEG P3-REF', 'EEG P4-REF', 'EEG O1-REF', 'EEG O2-REF', 'EEG T3-REF', 'EEG T4-REF', 'EEG T5-REF', 'EEG T6-REF', 'EEG A1-REF', 'EEG A2-REF', 'EEG FZ-REF', 'EEG CZ-REF', 'EEG PZ-REF', 'EEG EKG1-REF', 'EEG T1-REF', 'EEG T2-REF']
+directory_path='/home/azorzetto/data1/01_tcp_ar/01_tcp_ar'
+channels_to_set=['EEG FP1-REF', 'EEG FP2-REF', 'EEG F3-REF', 'EEG F4-REF', 'EEG C3-REF', 'EEG C4-REF', 'EEG P3-REF', 'EEG P4-REF', 'EEG O1-REF', 'EEG O2-REF', 'EEG T3-REF', 'EEG T4-REF', 'EEG T5-REF', 'EEG T6-REF', 'EEG A1-REF', 'EEG A2-REF', 'EEG FZ-REF', 'EEG CZ-REF', 'EEG PZ-REF', 'EEG T1-REF', 'EEG T2-REF']
 # List all files in the directory
 all_files = os.listdir(directory_path)
 # Filter out only EDF files
@@ -33,118 +34,114 @@ session_data: Dict[str, Dict[str, np.ndarray]] = defaultdict(lambda: defaultdict
 for file_name in sorted(edf_files):
     file_path = os.path.join(directory_path, file_name)
     sub_id, session, time = file_name.split(".")[0].split("_") #split the filname into subject, session and time frame
-    raw_mne = mne.io.read_raw_edf(file_path)  # Load the EDF file
-    raw_mne = raw_mne.reorder_channels(channels_to_set) #reorders the channels and drop the ones not contained in channels_to_set
-    raw_mne=raw_mne.resample(250) #resample to standardize sampling frequency to 250 Hz
+    raw_mne = mne.io.read_raw_edf(file_path, preload=True)  # Load the EDF file
+    raw_mne.pick_channels(channels_to_set) #reorders the channels and drop the ones not contained in channels_to_set
+    raw_mne.resample(250) #resample to standardize sampling frequency to 250 Hz
     epochs_mne = mne.make_fixed_length_epochs(raw_mne, duration=4, preload=True, overlap=3) #divide the signal into fixed lenght epoch of 4s with 1 second of overlapping: the overlapping starts from the left side of previous epoch
     epoch_data = epochs_mne.get_data() #trasform the raw eeg into a 3d np array
     epoch_data = np.expand_dims(epoch_data, 1) # number of epochs for that signal x 1 x channels x time samples 
 
     # If session_data[sub_id][session] exists, concatenate
     if session_data[sub_id][session].size > 0:
-        session_data[sub_id][session] = np.concatenate([session_data[sub_id][session], epoch_data], axis=-1)
+        session_data[sub_id][session] = np.concatenate([session_data[sub_id][session], epoch_data], axis=0)
     else:
         session_data[sub_id][session] = epoch_data
     
-    #data.append(epoch_data)
 
 
 #the normalization z2 is performed on each subject or session regardless the training/test set split. 
-# In our case it's the same because the trinaing set is composed by only 1 subject for now
-#in this case data should be a list of 4d arrays: each array is corresponding to a subject
-def normalization_z2(data):
-    for i, el in enumerate(data):
-        norm_el=normalization_z3(el)
-        if i==0:
-            final=norm_el
-        else:
-            final=np.concatenate((final, norm_el), axis=0)
-    return final
-
-session_data_normalized = defaultdict(lambda: defaultdict(list))
-for subj in session_data:
-    for session, listdata in session_data[subj]:
-        session_data_normalized[subj][session] = normalization_z3(data=listdata[0])
+# the normalization z3 is performed based on the entrie traing set 
 
 #leave one session out from the training for testing
 #we are loosing the subject information level
 def leave_one_session_out(session_data: Dict[str, Dict[str, np.ndarray]], normalization: str = None,shuffle: bool = True): #-> np.ndarray, np.ndarray, np.ndarray
     #a list of the dictionaries [{session: arrays}, {session: arrays}, {session: arrays},...]  
     if normalization == 'z2':
-        for subject, session_dict in session_data.items():
+        for subject, session_dict in session_data.items(): 
             # Collect all session arrays for this subject
-            sessions_list = (session_dict.values())
+            sessions_list = list(session_dict.values())
             # Concatenate all arrays to compute the mean and std
             total_for_subject = np.concatenate(sessions_list)
             mean = np.mean(total_for_subject)
             std = np.std(total_for_subject)
             # Normalize each session
-            for session_key in session_dict:
-                session_dict[session_key] = (session_dict[session_key] - mean) / std
-    list_sessions=session_data.keys()
-    list_dict_session=session_data.values(list_sessions)
+            for key in session_dict:
+                session_dict[key] = (session_dict[key] - mean) / std
+    #list of defautdict [{'session': array}]
+    list_dict_session=session_data.values() #the type is dict_values
     all_sessions=[] #a list containing all sessions
     for el in list_dict_session:
-        all_sessions=all_sessions.append(el.values())
-    test_size = int(0.2 * all_sessions.shape[0])
+
+        all_sessions.extend(list(el.values()))
+    test_size = int(np.ceil(0.2 * len(all_sessions)))
     test_data=all_sessions[0:test_size]
     train_val_data=all_sessions[test_size:]
     if normalization=='z3':
         total=np.concatenate(train_val_data)
         mean= np.mean(total)
         std=np.std(total)
-        for el in train_val_data:
-            el=el-mean
-            el=el/std
-    train_label = np.random.randint(0, 4, len(train_val_data)-1)
-    validation_label = np.random.randint(0, 4, 1)
+        for i in range(len(train_val_data)):
+            train_val_data[i] = (train_val_data[i] - mean) / std
+        mean_test=np.mean(np.concatenate(test_data))
+        std_test=np.std(np.concatenate(test_data))
+        for i in range(len(test_data)):
+            test_data[i] = (test_data[i] - mean_test) / std_test
+    train_label = np.random.randint(0, 4, len(train_val_data)-1) #because in the traingin dataset is the train_val_datset minus 1 session
+    validation_label = np.random.randint(0, 4, 1) #only one session in the val set
     #list of tuples containing the train data as the fist element and the validation data as the second element 
     combinations=[]
     for i in range (len(train_val_data)):
         if shuffle==True:
-            train_val_data[i]=random.shuffle(train_val_data[i])
+            random.shuffle(train_val_data[i])
         train_data = train_val_data[:i] + train_val_data[i+1:]
         val_data=train_val_data[i]
-        combinations.append((test_data, train_data, val_data))
+        combinations.append((test_data, train_data, val_data)) #(test_data: list, train_data: list, val_data: ndarray)
     
     return combinations, train_label, validation_label
 
 def leave_one_subject_out(session_data, normalization:str = None, shuffle: bool=True):
     subject_data_dict={}
-    for key, value in session_data.items():#key is the subj value is a dictionary 
+    for key, value in session_data.items():#key is the subj, value is a dictionary 
         # fixed key we are dealing with a subject 
-        new_value=np.concatenate(value.values()) #new value is the concatenation of a list of arrays representing the sessions for that subject
+        new_value=np.concatenate(list(value.values())) #new value is the concatenation of a list of arrays representing the sessions for that subject
         if normalization =='z2':
             mean=np.mean(new_value)
             std=np.std(new_value)
             new_value=(new_value-mean)/std
-
-    subject_data_dict= subject_data_dict.update({key: new_value})
-    all_data=subject_data_dict.values()
-    test_size = int(0.2 * all_data.shape[0])
+        subject_data_dict.update({key: new_value})
+    
+    all_data=list(subject_data_dict.values())
+    test_size = int(np.ceil(0.2 * len(all_data)))
     test_data=all_data[0:test_size]
     train_val_data=all_data[test_size:]
     if normalization=='z3':
         total=np.concatenate(train_val_data)
         mean= np.mean(total)
         std=np.std(total)
-        for el in train_val_data:
-            el=el-mean
-            el=el/std
+        for i in range(len(train_val_data)):
+            train_val_data[i]=train_val_data[i]-mean
+            train_val_data[i]=train_val_data[i]/std
+
+        mean_test=np.mean(np.concatenate(test_data))
+        std_test=np.std(np.concatenate(test_data))
+
+        for i in range(len(test_data)):
+            test_data[i] = (test_data[i] - mean_test) / std_test
+
     train_label = np.random.randint(0, 4, len(train_val_data)-1)
     validation_label = np.random.randint(0, 4, 1)
     combinations=[]
     for i in range (len(train_val_data)):
         if shuffle==True:
-            train_val_data[i]=random.shuffle(train_val_data[i])
+            random.shuffle(train_val_data[i])
         train_data = train_val_data[:i] + train_val_data[i+1:]
         val_data=train_val_data[i]
-        combinations.append((test_data, train_data, val_data))
+        combinations.append((test_data, train_data, val_data)) #list, list, ndarray
     
     return combinations, train_label, validation_label
 
-combinations1, train_label1, validation_label1= leave_one_session_out(session_data)
-combinations2, train_label2, validation_label2= leave_one_subject_out(session_data)
+combinations1, train_label1, validation_label1= leave_one_session_out(session_data, normalization='z3')
+combinations2, train_label2, validation_label2= leave_one_subject_out(session_data, normalization='z3')
 
 train_dataset = ds_time.EEG_Dataset(train_data, train_label, channels_to_set)
 validation_dataset = ds_time.EEG_Dataset(validation_data, validation_label, channels_to_set)
