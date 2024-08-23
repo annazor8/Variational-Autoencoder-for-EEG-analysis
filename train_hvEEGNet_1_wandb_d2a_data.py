20cfce88e0e2e3387ac7004689fd2ec45bd87eff
Anna <anna.zorzetto.1@studenti.unipd.it>
2024-08-23 12:25:12 +0200

    train con bci

--- train_hvEEGNet_1_wandb_d2a_data.py
+++ train_hvEEGNet_1_wandb_d2a_data.py
@@ -13,7 +13,10 @@ from library.training import wandb_training as wt
 from library.config import config_training as ct
 from library.config import config_model as cm
 from library.config import config_dataset as cd
-
+from library.dataset import preprocess as pp
+from library.model import hvEEGNet
+import torch
+import matplotlib.pyplot as plt
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Specific parameters to change inside the dictionary
 
@@ -21,8 +24,8 @@ from library.config import config_dataset as cd
 subj_list = [3]
 
 # Training parameters to change (for more info check the function get_config_hierarchical_vEEGNet_training)
-epochs = 2
-path_to_save_model = 'model_weights_backup'
+epochs = 80
+path_to_save_model = 'model_weights_backup_subj3'
 epoch_to_save_model = 1
 project_name = "Example_project"                # Name of wandb project
 name_training_run = "first_test_wandb"          # Name of the training run
@@ -59,3 +62,41 @@ train_config['use_classifier'] = model_config['use_classifier']
 
 # Train the model. 
 model = wt.train_wandb_V1('hvEEGNet_shallow', dataset_config, train_config, model_config)
+
+train_dataset, _, test_dataset = pp.get_dataset_d2a(dataset_config)
+# Get a random sample from the test dataset
+idx_trial = int(torch.randint(0, len(test_dataset), (1, 1)))
+x_eeg, label = test_dataset[idx_trial]
+
+# Add batch dimension
+x_eeg = x_eeg.unsqueeze(0)
+
+# Note on input size.
+# When you get a single signal from the dataset (as with the instruction x_eeg, label = test_dataset[idx]) the data you obtain has shape 1 x C x T
+# But hvEEGNet want an input of shape B x 1 x C x T. So for a single sample I have to add the batch dimension
+
+# EEG Reconstruction. To reconstruct an input signal you could use the function reconstruct, implemented inside hvEEGNet
+x_r_eeg = model.reconstruct(x_eeg)
+
+# Create time array (for the plot)
+t = torch.linspace(dataset_config['trial_start'], dataset_config['trial_end'], T)
+
+# Get the idx of channel to plot
+idx_ch = test_dataset.ch_list == ch_to_plot
+
+# Plot the original and reconstructed signal
+plt.rcParams.update({'font.size': 20})
+fig, ax = plt.subplots(1, 1, figsize = (12, 8))
+
+ax.plot(t, x_eeg.squeeze()[idx_ch].squeeze(), label = 'Original EEG', color = 'grey', linewidth = 2)
+ax.plot(t, x_r_eeg.squeeze()[idx_ch].squeeze(), label = 'Reconstructed EEG', color = 'black', linewidth = 1)
+
+ax.legend()
+ax.set_xlim([2, 4]) # Note that the original signal is 4s long. Here I plot only 2 second to have a better visualization
+ax.set_xlabel('Time [s]')
+ax.set_ylabel(r"Amplitude [$\mu$V]")
+ax.set_title("Subj {} (test) - Trial {} - Ch. {}".format(subj, idx_trial, ch_to_plot))
+ax.grid(True)
+
+fig.tight_layout()
+fig.show()
